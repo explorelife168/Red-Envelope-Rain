d333c220267b3603fed8e24c68e6e16e56be767b
Neil Wu <Neil@fakemail.com>
2024-04-11 21:53:13 +0800

    部署

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  // 靜態
+  // 靜態部署使用
   base: '/',
   resolve: {
     alias: {
