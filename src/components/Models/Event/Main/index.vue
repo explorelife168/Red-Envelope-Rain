ea7acb27e49458982f3a028b958ea244f0487c57
Neil Wu <Neil@fakemail.com>
2024-04-13 09:58:23 +0800

    banner setting

--- src/components/Models/Event/Main/index.vue
+++ src/components/Models/Event/Main/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="main-wrap">
     <Background />
+    <Homepage />
   </div>
 </template>
 
 <script setup lang="ts">
 import Background from '../../Event/global/Background/index.vue'
+import Homepage from '../Main/Homepage/index.vue'
 </script>
 
 <style scoped lang="scss">
