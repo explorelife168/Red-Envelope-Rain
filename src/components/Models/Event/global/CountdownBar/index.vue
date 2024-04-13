ddc50be0e47822d3384984ee8e51a25142fd7908
Neil Wu <Neil@fakemail.com>
2024-04-13 21:29:35 +0800

    countDownBar creating

--- src/components/Models/Event/global/CountdownBar/index.vue
+++ src/components/Models/Event/global/CountdownBar/index.vue
@@ -5,7 +5,12 @@
       'background-image': `url(${countdown_bg})`
     }"
   >
-    <div class="countdown-bar-blood"></div>
+    <div class="countdown-bar-blood">
+      <div class="text">
+        <p>{{ `${gettersCountDownName}04天02小時05分22秒 結束` }}</p>
+      </div>
+    </div>
+
     <div
       class="countdown-bar-cover"
       :style="{
@@ -18,6 +23,12 @@
 <script setup lang="ts">
 import countdown_bg from '@/assets/images/countdown_bg.png'
 import countdown_cover from '@/assets/images/countdown_cover.png'
+import { useCounterStore } from '@/stores/counter'
+import { computed } from 'vue'
+
+const useStore = useCounterStore()
+
+const gettersCountDownName = computed(() => useStore.gettersCountDownName)
 </script>
 <style scoped lang="scss">
 @import './index.scss';
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,13 +2,11 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
   const countDownName = ref('活動時間剩下')
-  const doubleCount = computed(() => count.value * 2)
-  const getterCountDownName = computed(() => countDownName.value)
-  function increment() {
-    count.value++
-  }
 
-  return { count, doubleCount, increment }
+  const gettersCountDownName = computed(() => {
+    return countDownName.value
+  })
+
+  return { countDownName, gettersCountDownName }
 })
