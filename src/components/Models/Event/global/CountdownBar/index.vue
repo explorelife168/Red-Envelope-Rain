e7850a11715fc6c97487d9961534c70a31c4cb5f
Neil Wu <Neil@fakemail.com>
2024-04-13 19:03:17 +0800

    countdown creating

---
+++ src/components/Models/Event/global/CountdownBar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="countdown-bar-container"
+    :style="{
+      'background-image': `url(${countdown_bg})`
+    }"
+  >
+    <div class="countdown-bar-blood"></div>
+    <div
+      class="countdown-bar-cover"
+      :style="{
+        'background-image': `url(${countdown_cover})`
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import countdown_bg from '@/assets/images/countdown_bg.png'
+import countdown_cover from '@/assets/images/countdown_cover.png'
+</script>
+<style scoped lang="scss">
+@import './index.scss';
+</style>
