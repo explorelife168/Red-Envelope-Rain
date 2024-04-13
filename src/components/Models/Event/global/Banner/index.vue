b3b3e4caa1be5c0611f3655b963602ade7af8b1b
Neil Wu <Neil@fakemail.com>
2024-04-13 09:57:33 +0800

    new banner

---
+++ src/components/Models/Event/global/Banner/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner-container">
+    <div
+      class="logo"
+      :style="{
+        'background-image': `url(${logo})`
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import logo from '@/assets/logo/logo.png'
+</script>
+<style scoped lang="scss">
+@import './index.scss';
+</style>
