6f17dfc33ca485221fe0c173ffe8fe502fc94e03
Neil Wu <Neil@fakemail.com>
2024-04-18 21:45:57 +0800

    add component

--- src/components/Models/Event/Main/Homepage/index.vue
+++ src/components/Models/Event/Main/Homepage/index.vue
@@ -2,11 +2,15 @@
   <div class="home-wrap">
     <Title />
     <CountdownBar />
+    <Scroll />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import CountdownBar from '../../global/CountdownBar/index.vue'
+import Footer from '../../global/Footer/index.vue'
+import Scroll from '../../global/Scroll/index.vue'
 import Title from '../../global/Title/index.vue'
 </script>
 
