648505c9fe2b327af9f54a1bb9741e9ae679ebe9
Neil Wu <Neil@fakemail.com>
2024-04-18 21:44:50 +0800

    footer

---
+++ src/components/Models/Event/global/Footer/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- <div class="footer-container"> -->
+  <div
+    class="footer-cloud"
+    :style="{
+      'background-image': `url(${footer_cloud})`
+    }"
+  ></div>
+  <div
+    class="footer-red-envelope"
+    :style="{
+      'background-image': `url(${footer_red_envelope})`
+    }"
+  ></div>
+  <!-- </div> -->
+</template>
+
+<script setup lang="ts">
+import footer_cloud from '@/assets/images/footer_1.png'
+import footer_red_envelope from '@/assets/images/footer_2.png'
+</script>
+<style scoped lang="scss">
+@import './index.scss';
+</style>
