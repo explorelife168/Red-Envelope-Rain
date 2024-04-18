da29585511ed50052afab92c9d8a4284baadcb4f
Neil Wu <Neil@fakemail.com>
2024-04-18 14:37:48 +0800

    add icon

--- src/components/Models/Event/global/Banner/index.vue
+++ src/components/Models/Event/global/Banner/index.vue
@@ -2,18 +2,34 @@
   <div class="banner-container">
     <div class="banner">
       <div class="border"></div>
+      <div
+        class="logo"
+        :style="{
+          'background-image': `url(${logo})`
+        }"
+      ></div>
+      <div
+        class="user-info"
+        :style="{
+          'background-image': `url(${user_icon})`
+        }"
+      >
+        <div class="user-info-id">會員編號 <span>00908001</span></div>
+      </div>
+      <div
+        class="game-info"
+        :style="{
+          'background-image': `url(${game_info})`
+        }"
+      ></div>
     </div>
     <div class="banner-block"></div>
-    <div
-      class="logo"
-      :style="{
-        'background-image': `url(${logo})`
-      }"
-    ></div>
   </div>
 </template>
 
 <script setup lang="ts">
+import game_info from '@/assets/images/game_info.png'
+import user_icon from '@/assets/images/user_icon.png'
 import logo from '@/assets/logo/logo.png'
 </script>
 <style scoped lang="scss">
